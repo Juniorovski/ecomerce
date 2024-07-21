c9582ab077a61ba5f85294a18dfe3d6908c40f4d
junio <juniornnine19@gmail.com>
2024-07-20 22:58:41 -0300

    update

--- frontend/ecomerce/src/components/card/index.tsx
+++ frontend/ecomerce/src/components/card/index.tsx
@@ -2,11 +2,16 @@ import { View, Text, Image, Pressable } from "react-native";
 import React, { useEffect, useState } from "react";
 import api from "@/api/api";
 
+
+
 export default function Card() {
   const [produtoData, setProdutoData] = useState([]);
 
-  useEffect(() => {
+  useEffect(()  => {
     fetchData();
+           
+        
+     
   }, []);
 
   const fetchData = async () => {
@@ -31,7 +36,7 @@ export default function Card() {
           />
         </Pressable>
        <View className="w-full flex flex-col items-center ">
-         <Text className="font-bold text-xl">{item.name.slice(0,14)}...</Text>
+         <Text className="font-bold text-xl">{item.name.slice(0,12)}...</Text>
           <Text className="color-slate-700 text-xl">{item.categoria.name}</Text>
         </View>
       </View>
